de98b644fed438203ee8b671fc184f4b8a94b38d
MatiasHernandezC <54877881+MatiasHernandezC@users.noreply.github.com>
2023-09-22 00:55:57 -0300

    Comienzo proyecto

---
+++ TopEducation/src/test/java/com/top/TopEducation/TopEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.top.TopEducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
